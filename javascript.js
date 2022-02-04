d8277cce64ad7fe730c42d2a33f9fbb73b6b294a
LassiterCoding <lassitercoding@gmail.com>
2022-02-03 22:13:28 -0500

    Began new repo for introductory javascript

---
+++ javascript.js
@@ -0,0 +1,37 @@
+let message = "Hello, World.";
+
+alert(message); // alerts "Hello, World."
+
+// Working with variables
+let admin, name; // declare 2 variables at once
+
+name = "John"
+
+admin = name;
+
+alert(admin); // alerts "John"
+
+// Correct Naming
+let ourPlanet = "Earth";
+
+let currentUser = "Timothy";
+
+// Arithmetic Operations
+let x = 100 + 50;
+
+alert(x); // alerts 150
+
+// JavaScript will try to convert strings to numbers in all numeric operations
+
+// let x = "100";
+// let y = "10";
+// let z = x / y; // returns 10
+
+// ASSIGNMENT
+console.log(23 + 97);
+
+let assignmentNumber = 3 + 4 + 7 + 2 + 5 + 8;
+
+console.log(assignmentNumber);
+
+alert((4 + 6 + 9) / 77);
